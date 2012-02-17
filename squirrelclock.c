56f545a6b65c103528cc24e0dfc72b0a25a8cfde
Mikael Voss <mikael@illdefined.org>
2012-02-17 23:28:02 +0000

    Use symbolic exit values

--- squirrelclock.c
+++ squirrelclock.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -58,5 +59,5 @@ int main(int argc, char *argv[]){
 		sleep(1);
 	}
 
-	return(0);
+	return EXIT_SUCCESS;
 }
